455dc09c2bfc9a8b2ce80ce84581bdc9235cd6cd
josebacarr@gmail.com <josebacarr@gmail.com@06e40e15-71bd-7a1b-1a1c-ba104c65ac7c>
2014-11-10 09:41:06 +0000

    rama desarrollo Joseba

---
+++ branches/joseba/src/com/ipartek/formacion/primero/HolaMundo.java
@@ -0,0 +1,46 @@
+package com.ipartek.formacion.primero;
+
+import com.ipartek.formacion.primero.bean.Libro;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		
+		//cAMBIO ANDER
+		//cambio Mario
+		String cadena = "Hola";
+		String nombre = "Pepe";
+		
+		int repeticion = 12;
+		
+		for (int i = 0; i < repeticion; i++) {
+			
+			//saludo con la varible 
+			System.out.println( cadena + " " + nombre + " " + i );
+			
+
+			
+		} 
+		
+		
+		//instanciar objeto de tipo Libro
+		Libro lib = new Libro();
+		
+		lib.setTitulo("titulo");
+		System.out.println(lib);
+		
+		
+		Libro lib2 = new Libro(10,"Java 7");
+		System.out.println(lib2);
+		
+		lib.setTitulo("Quijote");
+		System.out.println(lib.getTitulo());
+		
+		
+		
+		
+		
+		
+	}
+
+}
