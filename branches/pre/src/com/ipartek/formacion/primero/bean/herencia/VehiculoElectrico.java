fad65ee7f2e56d18e794f4330d5b5d2c5d7ff0a0
ander.ipartek@gmail.com <ander.ipartek@gmail.com@06e40e15-71bd-7a1b-1a1c-ba104c65ac7c>
2014-11-11 09:10:13 +0000

    Ejemplo de Herencia y Test

---
+++ branches/pre/src/com/ipartek/formacion/primero/bean/herencia/VehiculoElectrico.java
@@ -0,0 +1,51 @@
+package com.ipartek.formacion.primero.bean.herencia;
+
+
+public class VehiculoElectrico extends Vehiculo {
+
+	private float capacidadBaterias;
+	
+	public static final float MIN_POTENCIA = 150f;
+	
+	/**
+	 * Capacidad minima para una bateria
+	 */
+	static public final float MIN_CAP_BATERIA = 0; 
+	
+	public VehiculoElectrico() {
+		super();
+		setCapacidadBaterias(MIN_CAP_BATERIA);
+		setPotencia(VehiculoElectrico.MIN_POTENCIA);		
+	}
+	
+	public VehiculoElectrico( float potencia) {
+		//super();
+		setCapacidadBaterias(MIN_CAP_BATERIA);
+		setPotencia(potencia);
+	}
+	
+	@Override
+	public void setPotencia(float potencia) {		
+		super.setPotencia(potencia);
+	}
+	
+
+	public float getCapacidadBaterias() {
+		return capacidadBaterias;
+	}
+
+	public void setCapacidadBaterias(float capacidadBaterias) {
+		this.capacidadBaterias = capacidadBaterias;
+	}
+
+	@Override
+	public String toString() {
+		
+		return "VehiculoElectrico [capacidadBaterias=" + capacidadBaterias
+				+ ", numPlazas=" + numPlazas + ", dimensiones=" + dimensiones
+				+ ", potencia=" + potencia + "]";
+	}
+	
+	
+	
+}
