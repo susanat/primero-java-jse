2634bce8bbdd0f5946961d930162237f357ec1bb
ja.perez973@gmail.com <ja.perez973@gmail.com@06e40e15-71bd-7a1b-1a1c-ba104c65ac7c>
2014-11-11 09:50:16 +0000

    Ejemplos de herencias entre clases

--- src/com/ipartek/formacion/primero/bean/Alumno.java
+++ src/com/ipartek/formacion/primero/bean/Alumno.java
@@ -3,166 +3,55 @@ package com.ipartek.formacion.primero.bean;
 import java.util.ArrayList;
 
 /**
- * Clase para reprensentar un Alumno, se compone de:
- * <ol>
- * 	<li>nombre {@code String} obligatorio</li>
- * </ol>
- * 
- * 
- * @author Ander Uraga Real
+ * Hereda de la clase Persona
+ * Se compone de los siguientes campos
+ * <ul>
+ *   <li>calificaciones {@code ArrayList<Calificacion>}}
+ * </ul> 
+ * @author Jose A. Perez
  *
  */
-public class Alumno {
-	
-	static final char SEXO_VARON = 'v';
-	static final char SEXO_MUJER = 'm';
-	static final char SEXO_OTROS = 'o';
-	
-	static final int MAYOR_EDAD = 18;
-	
-	static final String NOMBRE_NULO = "sin determinar";
-	
-	
+public class Alumno extends Persona {
 	// Atributos
-	String nombre;
-	String apellido1;
-	String apellido2;
-	int edad;
-	String dni;
-	String poblacion;
-	boolean mayorEdad = false;
-	String telefono;
-	char sexo = 'o';	
-	ArrayList<Libro> libros;
-	ArrayList<Calificacion> calificaciones;
+	protected ArrayList<Calificacion> calificaciones;
 	
-	// Constructor
+	// Constructores
 	public Alumno() {
 		super();
-		this.nombre = "Jhon";
-		this.apellido1 = "Doe";
-		this.dni = "xxxxxxxxx";
+		// TODO Auto-generated constructor stub
 	}
-	
+
 	public Alumno(String nombre, String apellido1, String dni) {
-		super();
-		this.nombre = nombre;
-		this.apellido1 = apellido1;
-		this.dni = dni;
+		super(nombre, apellido1, dni);
+		// TODO Auto-generated constructor stub
 	}
 
-	
 	public Alumno(String nombre, String apellido1, int edad, String dni) {
-		super();
-		this.nombre = nombre;
-		this.apellido1 = apellido1;
-		this.edad = edad;
-		this.dni = dni;
-		this.mayorEdad = (this.edad>=18)? true: false; 
+		super(nombre, apellido1, edad, dni);
+		// TODO Auto-generated constructor stub
 	}
 
+	
 	// Getters y Setters
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getApellido1() {
-		return apellido1;
-	}
-
-	public void setApellido1(String apellido1) {
-		this.apellido1 = apellido1;
-	}
-
-	public String getApellido2() {
-		return apellido2;
-	}
-
-	public void setApellido2(String apellido2) {
-		this.apellido2 = apellido2;
-	}
-
-	public int getEdad() {
-		return edad;
-	}
-
-	public void setEdad(int edad) {
-		this.edad = edad;
-		this.mayorEdad = (this.edad>=MAYOR_EDAD)? true: false;
-	}
-
-	public String getDni() {
-		return dni;
-	}
-
-	public void setDni(String dni) {
-		this.dni = dni;
-	}
-
-	public String getPoblacion() {
-		return poblacion;
-	}
-
-	public void setPoblacion(String poblacion) {
-		this.poblacion = poblacion;
-	}
-
-	public boolean isMayorEdad() {
-		return mayorEdad;
-	}
-
-
-	
-
-
-	
-
-	public String getTelefono() {
-		return telefono;
-	}
-
-	public void setTelefono(String telefono) {
-		this.telefono = telefono;
-	}
-
-	public char getSexo() {
-		return sexo;
-	}
-
-	public void setSexo(char sexo) {
-		this.sexo = sexo;
-	}
-
-	public ArrayList<Libro> getLibros() {
-		return libros;
-	}
-
-	public void setLibros(ArrayList<Libro> libros) {
-		this.libros = libros;
-	}
-
 	public ArrayList<Calificacion> getCalificaciones() {
 		return calificaciones;
 	}
 
+
 	public void setCalificaciones(ArrayList<Calificacion> calificaciones) {
 		this.calificaciones = calificaciones;
 	}
-	
-	// Metodos
-		@Override
-		public String toString() {
-			return "Alumno [nombre=" + nombre + ", apellido1=" + apellido1
-					+ ", apellido2=" + apellido2 + ", edad=" + edad + ", dni="
-					+ dni + ", poblacion=" + poblacion + ", mayorEdad=" + mayorEdad
-					+ ", telefono=" + telefono + ", sexo=" + sexo + "]";
-		}
-	
-	
 
+	
+	//Metodos
+	@Override
+	public String toString() {
+		return "Alumno [calificaciones=" + calificaciones + ", nombre="
+				+ nombre + ", apellido1=" + apellido1 + ", apellido2="
+				+ apellido2 + ", edad=" + edad + ", dni=" + dni
+				+ ", poblacion=" + poblacion + ", mayorEdad=" + mayorEdad
+				+ ", telefono=" + telefono + ", sexo=" + sexo+"]";
+	}
+
+	
 }
