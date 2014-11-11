d47774da24dc1a0d60c10b7188fc1ab8f2be327b
aritz.justice@gmail.com <aritz.justice@gmail.com@06e40e15-71bd-7a1b-1a1c-ba104c65ac7c>
2014-11-11 09:15:37 +0000

    creado bean herencia con clases vehiculo y vehiculo electrico y modificacion de bean añadiendo un super persona a alumno y creando una clase formador q hereda de persona

--- branches/aritz/src/com/ipartek/formacion/primero/bean/Alumno.java
+++ branches/aritz/src/com/ipartek/formacion/primero/bean/Alumno.java
@@ -12,28 +12,11 @@ import java.util.ArrayList;
  * @author Ander Uraga Real
  *
  */
-public class Alumno {
-	
-	static final char SEXO_VARON = 'v';
-	static final char SEXO_MUJER = 'm';
-	static final char SEXO_OTROS = 'o';
-	
-	static final int MAYOR_EDAD = 18;
-	
-	static final String NOMBRE_NULO = "sin determinar";
+public class Alumno extends Persona{
 	
 	
 	// Atributos
-	String nombre;
-	String apellido1;
-	String apellido2;
-	int edad;
-	String dni;
-	String poblacion;
-	boolean mayorEdad = false;
-	String telefono;
-	char sexo = 'o';	
-	ArrayList<Libro> libros;
+	
 	ArrayList<Calificacion> calificaciones;
 	
 	// Constructor
@@ -62,90 +45,7 @@ public class Alumno {
 	}
 
 	// Getters y Setters
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getApellido1() {
-		return apellido1;
-	}
-
-	public void setApellido1(String apellido1) {
-		this.apellido1 = apellido1;
-	}
-
-	public String getApellido2() {
-		return apellido2;
-	}
-
-	public void setApellido2(String apellido2) {
-		this.apellido2 = apellido2;
-	}
-
-	public int getEdad() {
-		return edad;
-	}
-
-	public void setEdad(int edad) {
-		this.edad = edad;
-		this.mayorEdad = (this.edad>=MAYOR_EDAD)? true: false;
-	}
-
-	public String getDni() {
-		return dni;
-	}
-
-	public void setDni(String dni) {
-		this.dni = dni;
-	}
-
-	public String getPoblacion() {
-		return poblacion;
-	}
-
-	public void setPoblacion(String poblacion) {
-		this.poblacion = poblacion;
-	}
-
-	public boolean isMayorEdad() {
-		return mayorEdad;
-	}
-
-
 	
-
-
-	
-
-	public String getTelefono() {
-		return telefono;
-	}
-
-	public void setTelefono(String telefono) {
-		this.telefono = telefono;
-	}
-
-	public char getSexo() {
-		return sexo;
-	}
-
-	public void setSexo(char sexo) {
-		this.sexo = sexo;
-	}
-
-	public ArrayList<Libro> getLibros() {
-		return libros;
-	}
-
-	public void setLibros(ArrayList<Libro> libros) {
-		this.libros = libros;
-	}
-
 	public ArrayList<Calificacion> getCalificaciones() {
 		return calificaciones;
 	}
