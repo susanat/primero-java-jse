77180d89ab39e14a9b6b7a65c5cd426357d9d222
mario.alsol@gmail.com <mario.alsol@gmail.com@06e40e15-71bd-7a1b-1a1c-ba104c65ac7c>
2014-11-12 09:31:27 +0000

    -Cambio en las constructoras de las clases Figura, Cuadrado y Linea -Añadida la clase interna Punto a la clase Figura. -Añadido dos constantes (ORIGEN_X y ORIGEN_Y) en la clase Figura para establecer el punto de origen (0,0) -Añadida la Interface Redimensionable para guardar los valores LONG_MIN y LONG_MAX -Realizado los test para las constructoras de las clases Cuadrado y Linea

--- branches/mario/src/com/ipartek/formacion/primero/bean/herencia/Figura.java
+++ branches/mario/src/com/ipartek/formacion/primero/bean/herencia/Figura.java
@@ -2,72 +2,103 @@ package com.ipartek.formacion.primero.bean.herencia;
 
 /**
  * Clase Abstracta para realizar las tareas en común de sus hijos
+ * 
  * @author Mario Alvaro
  *
  */
 public abstract class Figura {
 	
-	//atributos
-	int x;
-	int y;
+	// atributos
+	// int x;
+	// int y;
+	public static final int ORIGEN_X = 0 ; 
+	public static final int ORIGEN_Y = 0 ; 
 	
-	//constructor
-	public Figura(int x, int y) {
+	Punto punto;
+	
+	
+	// constructor
+	public Figura() {
 		super();
-		setX(x);
-		setY(y);
+		punto = new Punto();
+	}
+	public Figura(Punto punto) {
+		super();
+		setPunto(punto);
 	}
 	
-	//getter y setters
-	public int getX() {
-		return x;
+	public Punto getPunto() {
+		return punto;
 	}
-	public void setX(int x) {
-		this.x = x;
+
+	public void setPunto(Punto punto) {
+		this.punto = punto;
 	}
-	public int getY() {
-		return y;
-	}
-	public void setY(int y) {
-		this.y = y;
-	}
-	
-	
-	//metodo implementado
+
+	// metodo implementado
 	/**
 	 * Mueve una <code>Figura</code> de su posicion actual a la nueva
-	 * @param x posicion x a mover
-	 * @param y posicion y a mover
+	 * 
+	 * @param x
+	 *            posicion x a mover
+	 * @param y
+	 *            posicion y a mover
+	 *            
 	 * @return true si es movida la <code>Figura</code>, false en caso contrario
 	 */
-	boolean mover(int x, int y){
+	boolean mover(int x, int y) {
+		// TODO comparar objeto completo con equals, en vez de comparar los
+		// atributos
 		boolean resul = false;
-		if ( ( getX() == x ) && ( getY() == y ) ) {
+		if ((punto.getX() == this.punto.x) && (punto.getY() == this.punto.y)) {
 			resul = true;
 		}
 		return resul;
 	}
-	
-	//metodo abstracto
+
+	// metodo abstracto
 
 	/**
 	 * Dibuja por pantalla la <code>Figura</code>
 	 */
 	abstract void dibujar();
-	
+
+	/**
+	 * Clase interna para trabajar con coordenadas X e Y
+	 * 
+	 * @author Mario Alvaro
+	 *
+	 */
+	protected class Punto {
+		int x;
+		int y;
+
+		protected Punto() {
+			this.x = ORIGEN_X;
+			this.y = ORIGEN_Y;
+		}
+
+		protected Punto(int x, int y) {
+			setX(x);
+			setY(y);
+		}
+
+		public int getX() {
+			return x;
+		}
+
+		public void setX(int x) {
+			this.x = x;
+		}
+
+		public int getY() {
+			return y;
+		}
+
+		public void setY(int y) {
+			this.y = y;
+		}
+
+	}
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
