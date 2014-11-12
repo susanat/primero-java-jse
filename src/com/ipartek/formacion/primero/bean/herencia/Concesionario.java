edea61f92b35bbe61347e96be685d1af5fa121e9
mario.alsol@gmail.com <mario.alsol@gmail.com@06e40e15-71bd-7a1b-1a1c-ba104c65ac7c>
2014-11-12 11:32:41 +0000

    -Añadido variables de ventaGlobal y ventas a la clase Concesionario con sus getters y setters -Realizado test de la clase Concesionario para ver que la variable ventaGloba es la misma al usar 2 instancias diferentes

--- src/com/ipartek/formacion/primero/bean/herencia/Concesionario.java
+++ src/com/ipartek/formacion/primero/bean/herencia/Concesionario.java
@@ -2,25 +2,69 @@ package com.ipartek.formacion.primero.bean.herencia;
 
 /**
  * Objeto para poder comprar Vehiculo
+ *
  * @author Mario Alvaro
  *
  */
 public class Concesionario {
+	public static final int SINVENTAS = 0;
+	/**
+	 * Atributo <code>static</code> para llevar el contador de
+	 * <code>Vehiculos</code> vendidos
+	 */
+	private static int ventasGlobales;
+	/**
+	 * Atributo no estatico, para las ventas de cada <code>Concesionario</code>
+	 */
+	private int ventas;
 
 	/**
-	 * compramos un vehiculo que puede ser <code>VehiculoElectrico</code> o <code>VehiculoGasolina</code>
-	 * @param electrico - true <code>VehiculoElectrico</code>, false <code>VehiculoGasolina</code>
+	 * Constructor vacio, inicializa las ventas a cero
+	 */
+	public Concesionario() {
+		super();
+		setVentas(SINVENTAS);
+		setVentasGlobales(SINVENTAS);
+	}
+
+	// getter y setters
+
+	public int getVentasGlobales() {
+		return ventasGlobales;
+	}
+
+	public void setVentasGlobales(int ventaGlob) {
+		ventasGlobales = ventaGlob;
+	}
+
+	public int getVentas() {
+		return ventas;
+	}
+
+	public void setVentas(int ventas) {
+		this.ventas = ventas;
+	}
+
+	/**
+	 * compramos un vehiculo que puede ser <code>VehiculoElectrico</code> o
+	 * <code>VehiculoGasolina</code>
+	 *
+	 * @param electrico
+	 *            - true <code>VehiculoElectrico</code>, false
+	 *            <code>VehiculoGasolina</code>
 	 * @return <code>Vehiculo</code> comparado
 	 */
-	static public Vehiculo comprarVehiculo(boolean electrico){
+	public Vehiculo comprarVehiculo(boolean electrico) {
+		ventasGlobales++;
+		ventas++;
 		Vehiculo resul = null;
-		
-		if ( electrico ) {
+
+		if (electrico) {
 			resul = new VehiculoElectrico();
-		}else{
+		} else {
 			resul = new VehiculoGasolina();
 		}
 		return resul;
 	}
-	
+
 }
