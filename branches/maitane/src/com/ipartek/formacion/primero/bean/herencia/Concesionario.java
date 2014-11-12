11000241a72631a82f67f54d1bd7b8cff391b140
maitanecasfer@gmail.com <maitanecasfer@gmail.com@06e40e15-71bd-7a1b-1a1c-ba104c65ac7c>
2014-11-12 11:32:09 +0000

    crear variable static en Concesionario. crear un nuevo test para comprbar las ventas  globales y por concesionario

--- branches/maitane/src/com/ipartek/formacion/primero/bean/herencia/Concesionario.java
+++ branches/maitane/src/com/ipartek/formacion/primero/bean/herencia/Concesionario.java
@@ -2,24 +2,71 @@ package com.ipartek.formacion.primero.bean.herencia;
 
 /**
  * Objeto para poder comprar Vehiculo
+ *
  * @author Ander Uraga Real
  *
  */
 public class Concesionario {
-	
+
 	/**
-	 * compramos un vehiculo que puede ser <code>VehiculoElectrico</code> o <code>VehiculoGasolina</code>
-	 * @param electrico true <code>VehiculoElectrico</code>, false <code>VehiculoGasolina</code>
+	 * Atributo estatico para llevar el contador de vehiculos vendidos
+	 */
+
+	private static int ventasGlobales = 0;
+	/**
+	 * Atributo no estatico para las ventas de cada Concesionario
+	 */
+	private int ventas = 0;
+
+	/**
+	 * Constructor vacio que inicializa las ventas a 0
+	 */
+
+	public Concesionario() {
+		super();
+		this.ventas = 0;
+	}
+
+	// getters y setters
+
+	public static int getVentasGlobales() {
+		return ventasGlobales;
+	}
+
+	public int getVentas() {
+		return ventas;
+	}
+
+	/**
+	 * Inicializa el contador ventasGlobales a 0
+	 * 
+	 * @param ventasGlobales
+	 */
+
+	public static void setVentasGlobales(int ventasGlobales) {
+		Concesionario.ventasGlobales = ventasGlobales;
+	}
+
+	/**
+	 * compramos un vehiculo que puede ser <code>VehiculoElectrico</code> o
+	 * <code>VehiculoGasolina</code>
+	 *
+	 * @param electrico
+	 *            true <code>VehiculoElectrico</code>, false
+	 *            <code>VehiculoGasolina</code>
 	 * @return <code>Vehiculo</code> comprado
 	 */
-	static public Vehiculo comprarVehiculo( boolean electrico ){
-		
+	public Vehiculo comprarVehiculo(boolean electrico) {
+
+		ventas++;
+		ventasGlobales++;
+
 		Vehiculo resul = null;
-		if ( electrico ){
+		if (electrico) {
 			resul = new VehiculoElectrico();
-		}else{
+		} else {
 			resul = new VehiculoGasolina();
-		}		
+		}
 		return resul;
 	}
 
--- branches/maitane/test/com/ipartek/formacion/primero/bean/herencia/ConcesionarioTest.java
+++ branches/maitane/test/com/ipartek/formacion/primero/bean/herencia/ConcesionarioTest.java
@@ -1,6 +1,9 @@
 package com.ipartek.formacion.primero.bean.herencia;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -9,6 +12,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ConcesionarioTest {
+	Concesionario concesionario = null;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -20,52 +24,100 @@ public class ConcesionarioTest {
 
 	@Before
 	public void setUp() throws Exception {
+		concesionario = new Concesionario();
+		Concesionario.setVentasGlobales(0);
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		concesionario = null;
 	}
 
 	@Test
 	public void testComprarCoche() {
-		//usar la palabra reservada instanceof
-		
-		Vehiculo ve = Concesionario.comprarVehiculo(true);  //electrico
-		Vehiculo vg = Concesionario.comprarVehiculo(false); //gasolina
-		
-		//test que es vehiculo electrico y no gasolina
-		assertTrue ( ve instanceof VehiculoElectrico );
-		assertFalse( ve instanceof VehiculoGasolina );
-		assertTrue ( ve instanceof Vehiculo );
-		
-		
-		assertFalse ( vg instanceof VehiculoElectrico );
-		assertTrue  ( vg instanceof VehiculoGasolina );
-		assertTrue  ( vg instanceof Vehiculo );
-		
-		
+		// usar la palabra reservada instanceof
+
+		Vehiculo ve = concesionario.comprarVehiculo(true); // electrico
+		Vehiculo vg = concesionario.comprarVehiculo(false); // gasolina
+
+		// test que es vehiculo electrico y no gasolina
+		assertTrue(ve instanceof VehiculoElectrico);
+		assertFalse(ve instanceof VehiculoGasolina);
+		assertTrue(ve instanceof Vehiculo);
+
+		assertFalse(vg instanceof VehiculoElectrico);
+		assertTrue(vg instanceof VehiculoGasolina);
+		assertTrue(vg instanceof Vehiculo);
+
 	}
-	
+
 	/**
-	 * Queremos testear que si compro un VehiculoElectrico
-	 * pueda saber la CapacidadBaterias
+	 * Queremos testear que si compro un VehiculoElectrico pueda saber la
+	 * CapacidadBaterias
 	 */
 	@Test
 	public void testComprarCocheElectrico() {
-		
-		Vehiculo ve = Concesionario.comprarVehiculo(true);
-		assertTrue ("No es una instancia de VehiculoElectrico", ve instanceof VehiculoElectrico );
-		
-		if ( ve instanceof VehiculoElectrico ){
-			//castear  de Vehiculo a VehiculoElectrico 
+
+		Vehiculo ve = concesionario.comprarVehiculo(true);
+		assertTrue("No es una instancia de VehiculoElectrico",
+				ve instanceof VehiculoElectrico);
+
+		if (ve instanceof VehiculoElectrico) {
+			// castear de Vehiculo a VehiculoElectrico
 			VehiculoElectrico ve2 = (VehiculoElectrico) ve;
-			ve2.getCapacidadBaterias();			
-			assertTrue (true);
-						
-			//casteo directo
+			ve2.getCapacidadBaterias();
+			assertTrue(true);
+
+			// casteo directo
 			((VehiculoElectrico) ve).getCapacidadBaterias();
-			assertTrue (true);			
-		}	
+			assertTrue(true);
+		}
 	}
 
+	/**
+	 * Test para comprobar el numero de ventas por Concesionario y
+	 * ventasGlobales
+	 * <ul>
+	 * <li>Instanciar 2 objetos de tipo <code>Concesionario</code></li>
+	 * <li>Objeto1 vende 3 vehiculos</li>
+	 * <li>Objeto2 vende 5 vehiculos</li>
+	 * <li>test ventas ==3 en objeto1</li>
+	 * <li>test ventas ==5 en objeto2</li>
+	 * <li>test ventasGlobales ==8 en objeto1 y objeto2</li>
+	 * </ul>
+	 */
+
+	@Test
+	public void testVentas() {
+
+		for (int i = 0; i < 3; i++) {
+			concesionario.comprarVehiculo(true);
+
+		}
+
+		Concesionario concesionario2 = new Concesionario();
+
+		for (int i = 0; i < 5; i++) {
+			concesionario2.comprarVehiculo(true);
+		}
+
+		assertEquals(
+				"El numero de vehiculos vendidos en concesionario 1 no es correcto",
+				concesionario.getVentas(), 3);
+		assertEquals(
+				"El numero de vehiculos vendidos en concesionario 2 no es correcto",
+				concesionario2.getVentas(), 5);
+
+		// ventas globales
+		assertEquals(
+				"El numero de vehiculos vendidos en todos los concesionario no es correcto",
+				Concesionario.getVentasGlobales(), 8);
+		assertSame(
+				"El numero de vehiculos vendidos en todos los concesionario no es correcto",
+				8, concesionario.getVentasGlobales());
+		assertSame(
+				"El numero de vehiculos vendidos en todos los concesionario no es correcto",
+				8, concesionario2.getVentasGlobales());
+
+	}
 }
