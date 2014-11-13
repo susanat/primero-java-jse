a8947edb5f4847bec94fc6152f66fd12246dab47
ander.ipartek@gmail.com <ander.ipartek@gmail.com@06e40e15-71bd-7a1b-1a1c-ba104c65ac7c>
2014-11-13 09:44:54 +0000

    Ejemplos con las interfaces Comparable y Comparator

---
+++ src/com/ipartek/formacion/primero/bean/herencia/Arrancable2.java
@@ -0,0 +1,8 @@
+package com.ipartek.formacion.primero.bean.herencia;
+
+public interface Arrancable2 {
+
+	static final int VAR = 2;
+
+	boolean arrancar();
+}
---
+++ src/com/ipartek/formacion/primero/bean/herencia/Ordenable.java
@@ -0,0 +1,6 @@
+package com.ipartek.formacion.primero.bean.herencia;
+
+public interface Ordenable {
+
+	int getPeso();
+}
