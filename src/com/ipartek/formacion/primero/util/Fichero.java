643ab22bd01ff5ac7055e5f34926738ae481c37e
mario.alsol@gmail.com <mario.alsol@gmail.com@06e40e15-71bd-7a1b-1a1c-ba104c65ac7c>
2014-11-14 12:04:58 +0000

    Nueva clase para manejo de ficheros

---
+++ src/com/ipartek/formacion/primero/util/Fichero.java
@@ -0,0 +1,60 @@
+package com.ipartek.formacion.primero.util;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+public class Fichero {
+
+	/**
+	 * Crea un fichero de texto con codificacion "utf-8"
+	 *
+	 * @param nombreFichero
+	 *            - path + nombre + extension --> ej:filename.text
+	 *
+	 * @param contenido
+	 *            - contenido a escribir en el fichero
+	 *
+	 * @return TRUE si se crea el fichero, FALSE en caso contrario
+	 */
+
+	public static boolean create(String nombreFichero, String contenido) {
+		boolean resul = false;
+
+		Writer writer = null;
+		FileOutputStream ficheroTexto = null;
+		OutputStreamWriter outputStream = null;
+		try {
+			// Creamos el nombre y extension del fichero
+			ficheroTexto = new FileOutputStream(nombreFichero);
+
+			// Declaramos un Stream para escribir
+			// pasamos como parametros el fichero a escribir y su Charset
+			outputStream = new OutputStreamWriter(ficheroTexto, "utf-8");
+
+			// Objeto Writer para escribir
+			// mejoramos el rendimiento con un buffer
+			writer = new BufferedWriter(outputStream);
+
+			// escribir en el fichero a traves del writer
+			writer.write(contenido);
+			resul = true;
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+
+		} finally {
+			try {
+				writer.close();
+
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+
+		return resul;
+	}
+
+}
