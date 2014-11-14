43e4e40d87219ffa136f1b9e8090165b347c58bb
mario.alsol@gmail.com <mario.alsol@gmail.com@06e40e15-71bd-7a1b-1a1c-ba104c65ac7c>
2014-11-14 11:53:49 +0000

    Añadido ejemplo para crear archivo de texto con la clase CrearFicheroTexto

---
+++ src/com/impartek/formacion/primero/streams/CrearFicheroTexto.java
@@ -0,0 +1,46 @@
+package com.impartek.formacion.primero.streams;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+public class CrearFicheroTexto {
+
+	public CrearFicheroTexto() {
+
+	}
+
+	public static void main(String[] args) {
+
+		Writer writer = null;
+		FileOutputStream ficheroTexto = null;
+		OutputStreamWriter outputStream = null;
+		try {
+			// Creamos el nombre y extension del fichero
+			ficheroTexto = new FileOutputStream("filename.txt");
+			// Declaramos un Stream para escribir
+			// pasamos como parametros el fichero a escribir y su Charset
+			outputStream = new OutputStreamWriter(ficheroTexto, "utf-8");
+
+			// Objeto Writer para escribir
+			// mejoramos el rendimiento con un buffer
+			writer = new BufferedWriter(outputStream);
+
+			// escribir en el fichero a traves del writer
+			writer.write("Hello World");
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+
+		} finally {
+			try {
+				writer.close();
+
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+}
