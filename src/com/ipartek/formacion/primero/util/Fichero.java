005dc6b583e38b6501b41c8a9c5944fdd9a74561
ja.perez973@gmail.com <ja.perez973@gmail.com@06e40e15-71bd-7a1b-1a1c-ba104c65ac7c>
2014-11-14 12:25:04 +0000

    Ejemplos con Ficheros de texto

---
+++ src/com/ipartek/formacion/primero/util/Fichero.java
@@ -0,0 +1,75 @@
+package com.ipartek.formacion.primero.util;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+public class Fichero {
+
+	/**
+	 * Crea un fichero de texto con codificacion "utf-8"
+	 *
+	 * @param nombreFichero
+	 *            path + nombre + extension, ej:filename.txt
+	 * @param contenido
+	 *            contenido a escribir en el fichero
+	 * @return true si se crea el fichero, false en caso contrario
+	 */
+	public static boolean create(String nombreFichero, String contenido) {
+		boolean resul = false;
+
+		Writer writer = null;
+		FileOutputStream ficheroTexto = null;
+		OutputStreamWriter outputStream = null;
+
+		try {
+			// Creamos el nombre y extension del fichero
+			ficheroTexto = new FileOutputStream(nombreFichero);
+
+			// declaramos un Stream para escribir
+			// pasamos como parametros el fichero a escribir y su Charset
+			outputStream = new OutputStreamWriter(ficheroTexto, "utf-8");
+
+			// objeto Writer para escribir
+			// mejoramos el rendimiento con un buffer
+			writer = new BufferedWriter(outputStream);
+
+			// escribir en el fichero a traves del writer
+			writer.write(contenido);
+			resul = true;
+		}
+
+		catch (IOException ex) {
+			ex.printStackTrace();
+		}
+
+		finally {
+			try {
+				writer.close();
+			}
+
+			catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+
+		return resul;
+	}
+
+	public static boolean remove(String ficheroName) {
+		boolean resul = false;
+
+		try {
+			File file = new File(ficheroName);
+			resul = file.delete();
+		}
+
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return resul;
+	}
+}
---
+++ test/com/ipartek/formacion/primero/util/FicheroTest.java
@@ -0,0 +1,39 @@
+package com.ipartek.formacion.primero.util;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class FicheroTest {
+
+	static final String FICHERO_NAME_TEST1 = "filename.txt";
+	static final String FICHERO_CONTENIDO_TEST1 = "Hello World";
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		assertTrue(FICHERO_NAME_TEST1 + ": No se puedo eliminar el fichero",
+				Fichero.remove(FICHERO_NAME_TEST1));
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCreate() {
+		assertTrue(Fichero.create(FICHERO_NAME_TEST1, FICHERO_CONTENIDO_TEST1));
+	}
+
+}
