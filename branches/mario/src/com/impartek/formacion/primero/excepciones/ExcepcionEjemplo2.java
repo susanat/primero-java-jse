9b96f6c3d99defdd3d421f8647584435fd91664d
mario.alsol@gmail.com <mario.alsol@gmail.com@06e40e15-71bd-7a1b-1a1c-ba104c65ac7c>
2014-11-14 08:44:37 +0000

    -Añadida clase ExceptionEjemplo2 para practicar con diferentes excepciones

---
+++ branches/mario/src/com/impartek/formacion/primero/excepciones/ExcepcionEjemplo2.java
@@ -0,0 +1,75 @@
+package com.impartek.formacion.primero.excepciones;
+
+import com.ipartek.formacion.primero.bean.herencia.Vehiculo;
+import com.ipartek.formacion.primero.bean.herencia.VehiculoElectrico;
+import com.ipartek.formacion.primero.bean.herencia.VehiculoGasolina;
+
+public class ExcepcionEjemplo2 {
+	// propiedades necesarias
+
+	static int array_corto[] = new int[5];
+
+	public static void main(String args[]) {
+
+		try {
+
+			System.out.println();
+
+			System.out.println(".antes de la excepcion");
+
+			array_corto[4] = 10;
+
+			// pete para AritmeticException
+			// int resul = 10 / 0;
+
+			// pete para NullPointerException
+			// String nulo = null;
+			// nulo.length();
+
+			// pete por una Exception que no haya sido expecificada
+			Vehiculo vg = new VehiculoElectrico();
+			((VehiculoGasolina) vg).isGasolina();
+
+			System.out.println(".no hay una excepcion");
+		}
+
+		catch (ArrayIndexOutOfBoundsException e) {
+
+			// se captura una excepción por intentar acceder a una posición
+			// fuera de los límites del array.
+
+			System.out.println(".hay una excepcion por acceso incorrecto");
+
+		}
+
+		catch (ArithmeticException e) {
+
+			// se captura una excepción aritmética.
+
+			System.out.println(".hay una excepcion por error aritmetico");
+
+		}
+
+		catch (NullPointerException e) {
+			System.out.println("NullPointerException " + e.getMessage());
+			e.printStackTrace();
+		}
+
+		catch (Exception e) {
+			System.out.println("Excepcion general: ");
+			System.out.println("Message: " + e.getMessage());
+			e.printStackTrace();
+
+		}
+
+		finally {
+
+			// después del try catch...
+
+			System.out.println(".siempre se ejecuta [finally]");
+
+		}
+
+	}
+
+}
