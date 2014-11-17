8a823dc98d64a52c207726e888e912d2c754099e
maitanecasfer@gmail.com <maitanecasfer@gmail.com@06e40e15-71bd-7a1b-1a1c-ba104c65ac7c>
2014-11-17 12:24:04 +0000

    Ejercicios cn formatos de fechas. Creacion de la enum Idioma

---
+++ src/com/ipartek/formacion/primero/util/Idioma.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.primero.util;
+
+/**
+ * Enumeracion para los Idiomas
+ * 
+ * @author Maitane Casado Fernandez
+ *
+ */
+
+public enum Idioma {
+
+	CASTELLANO, EUSKERA;
+}
