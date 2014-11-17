eed9c766ec3766c69d0c78d67fd30c0272683a66
maitanecasfer@gmail.com <maitanecasfer@gmail.com@06e40e15-71bd-7a1b-1a1c-ba104c65ac7c>
2014-11-17 12:23:28 +0000

    Ejercicios cn ficheros

--- test/com/ipartek/formacion/primero/util/FicheroTest.java
+++ test/com/ipartek/formacion/primero/util/FicheroTest.java
@@ -1,6 +1,7 @@
 package com.ipartek.formacion.primero.util;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -11,7 +12,10 @@ import org.junit.Test;
 public class FicheroTest {
 
 	static final String FICHERO_NAME_TEST1 = "filename.txt";
-	static final String FICHERO_CONTENT_TEST1 = "Hello World";
+	static final String FICHERO_CONTENT_TEST1 = "Hello Wolrd";
+	static final String FICHERO_CONTENT_TEST2 = "filename.txt \nlinea1 \nlinea2";
+
+	static final int BUCLE = 10000;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -19,7 +23,8 @@ public class FicheroTest {
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
-		assertTrue("No se ha podidio eliminar " + FICHERO_NAME_TEST1,
+
+		assertTrue(FICHERO_NAME_TEST1 + " No se pudo eliminar",
 				Fichero.remove(FICHERO_NAME_TEST1));
 	}
 
@@ -33,7 +38,46 @@ public class FicheroTest {
 
 	@Test
 	public void testCreate() {
+
 		assertTrue(Fichero.create(FICHERO_NAME_TEST1, FICHERO_CONTENT_TEST1));
+
+	}
+
+	@Test
+	public void testRead() {
+
+		try {
+
+			assertTrue(Fichero
+					.create(FICHERO_NAME_TEST1, FICHERO_CONTENT_TEST2));
+			Fichero.read(FICHERO_NAME_TEST1);
+
+		} catch (Exception e) {
+			fail("Exception leyendo fichero");
+		}
+
+	}
+
+	@Test(timeout = 1000)
+	public void testWriteString() {
+		String content = "";
+		for (int i = 0; i < BUCLE; i++) {
+			content += "linea" + i + "\n";
+		}
+		Fichero.create(FICHERO_NAME_TEST1, content);
+		// Fichero.read(FICHERO_NAME_TEST1);
+		assertTrue(Fichero.remove(FICHERO_NAME_TEST1));
+	}
+
+	@Test(timeout = 1000)
+	public void testWriteStringBuilder() {
+		StringBuilder sbContent = new StringBuilder();
+		for (int i = 0; i < BUCLE; i++) {
+			sbContent.append("linea" + i + "\n");
+		}
+		Fichero.create(FICHERO_NAME_TEST1, sbContent.toString());
+		// Fichero.read(FICHERO_NAME_TEST1);
+		assertTrue(Fichero.remove(FICHERO_NAME_TEST1));
 	}
 
 }
