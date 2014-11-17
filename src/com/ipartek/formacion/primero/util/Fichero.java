3f12622f76eb280ba9df91dcf315f29167af2ab1
mario.alsol@gmail.com <mario.alsol@gmail.com@06e40e15-71bd-7a1b-1a1c-ba104c65ac7c>
2014-11-17 08:18:20 +0000

    Añadido test para leer ficheros

--- src/com/ipartek/formacion/primero/util/Fichero.java
+++ src/com/ipartek/formacion/primero/util/Fichero.java
@@ -1,8 +1,10 @@
 package com.ipartek.formacion.primero.util;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
@@ -58,10 +60,63 @@ public class Fichero {
 		return resul;
 	}
 
-	public static boolean remove(String ficheroNameTest1) {
+	/**
+	 * Lee el fichero y lo muestra por pantalla
+	 *
+	 * @param ficherName
+	 *            path y nombre del fichero
+	 */
+	public static void read(String ficherName) {
+
+		BufferedReader br = null; // buffer para mejorar la lectura
+		FileReader reader = null;
+		try {
+
+			String sCurrentLine; // Para leer las lineas del fichero
+
+			// Crear un Stream de lectura
+			reader = new FileReader(ficherName);
+			// Asociar el buffer al Stream
+			br = new BufferedReader(reader);
+
+			// Bucle para leer linea a linea
+			while ((sCurrentLine = br.readLine()) != null) {
+				System.out.println(sCurrentLine);
+			}
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+
+			try {
+
+				if (br != null) {
+					br.close();
+				}
+
+			} catch (IOException ex) {
+
+				ex.printStackTrace();
+
+			}
+
+		}
+	}
+
+	/**
+	 * Eliminar fichero
+	 *
+	 * @param ficheroName
+	 *            - path y nombre del fichero
+	 *
+	 * @return true si es eliminado, false en caso contrario
+	 */
+	public static boolean remove(String ficheroName) {
 		boolean resul = false;
 		try {
-			File fichero = new File(ficheroNameTest1);
+			File fichero = new File(ficheroName);
 			resul = fichero.delete();
 
 		} catch (Exception ex) {
