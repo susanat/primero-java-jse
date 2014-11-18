346a826514616b3c5e8d38ef2bb9dae48b05b955
ander.ipartek@gmail.com <ander.ipartek@gmail.com@06e40e15-71bd-7a1b-1a1c-ba104c65ac7c>
2014-11-18 10:50:02 +0000

    funcion para mostrar estadisticas de las tiradas

--- src/com/ipartek/formacion/primero/util/Dado.java
+++ src/com/ipartek/formacion/primero/util/Dado.java
@@ -2,22 +2,55 @@ package com.ipartek.formacion.primero.util;
 
 public class Dado {
 
-	static String[] alumnos = new String[] { "Susana", "Zuahitz", "Kepa",
-			"Urko", "Jon", "Sergio", "Joseba", "Mario", "Aritz", "Fran",
-			"Maitane", "Jose" };
+	public static final String[] ALUMNOS_TIRADA = new String[] { "Susana",
+		"Zuahitz", "Kepa", "Urko", "Jon", "Sergio", "Joseba", "Mario",
+		"Aritz", "Fran", "Maitane", "Jose" };
 
 	static public void tirar() {
 		int aleatorio = getTirada();
-		System.out.println(alumnos[aleatorio]);
+		System.out.println(ALUMNOS_TIRADA[aleatorio]);
 	}
 
 	static public int getTirada() {
-		return (int) (Math.random() * alumnos.length);
+		return (int) (Math.random() * ALUMNOS_TIRADA.length);
 	}
 
+	
+
+	/**
+	 * Muestra por pantalla una tabla con todos los participantes y el numero de
+	 * veces que han salido elegidos
+	 *  
+	 *      --------------------------------------
+	 *      Participantes          |      Tiradas
+	 *      --------------------------------------
+	 *      Susana                             12
+	 *      Zuahitz                            98
+	 *      Fran                            1.112
+	 *      etc                               etc
+	 *      -------------------------------------
+	 *      13 Participantes                2.345  
+	 *      -------------------------------------
+	 * 
+	 * @param numTiradas
+	 *            numero de veces a tirar el Dado
+	 */
+	
+	
+	
+	
+	public static void estatidisticas(int numTiradas) {
+ 
+		//TODO usar un HashMap 
+		System.out.println("Estadisticas");
+	}
+	
+	
 	public static void main(String[] args) {
 		tirar();
+		estatidisticas(1);
 
 	}
+	
 
 }
