1ca6b4a411adfba738263d19206f9303d6541517
ander.ipartek@gmail.com <ander.ipartek@gmail.com@06e40e15-71bd-7a1b-1a1c-ba104c65ac7c>
2014-11-18 08:02:49 +0000

    nuevos ejemplos con Fechas

--- test/com/ipartek/formacion/primero/util/APITest.java
+++ test/com/ipartek/formacion/primero/util/APITest.java
@@ -4,8 +4,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.Objects;
 import java.util.StringTokenizer;
 
@@ -38,8 +36,6 @@ public class APITest {
 	String lineaPersona = personaNombre + separador + personaApellido1
 			+ separador + personaApellido2 + separador + personaEdad;
 
-	GregorianCalendar hoyDate = null;
-
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -50,15 +46,12 @@ public class APITest {
 
 	@Before
 	public void setUp() throws Exception {
-		// obtener Fecha actual con new Date
-		hoyDate = new GregorianCalendar();
-		hoyDate.setTime(new Date());
 
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		hoyDate = null;
+
 	}
 
 	@Test
@@ -152,58 +145,6 @@ public class APITest {
 
 		String cadenaCambiada2 = cadenaOriginal2.replace("Hola", "Adios");
 		assertEquals(cadenaReemplazada2, cadenaCambiada2);
-
-	}
-
-	@Test
-	public void testDate() throws Exception {
-
-		// obtener Fecha actual con System.currentTimeMillis()
-		GregorianCalendar hoyCurrent = new GregorianCalendar();
-		hoyCurrent.setTimeInMillis(System.currentTimeMillis());
-
-		// test las dos fechas iguales
-		assertEquals(hoyDate, hoyCurrent);
-		assertEquals(hoyDate.getTimeInMillis(), hoyCurrent.getTimeInMillis());
-
-		// Dia del mes
-		assertEquals(17, hoyDate.get(GregorianCalendar.DAY_OF_MONTH));
-
-		// Dia de la semana
-		assertEquals(GregorianCalendar.MONDAY,
-				hoyDate.get(GregorianCalendar.DAY_OF_WEEK));
-		assertEquals(2, hoyDate.get(GregorianCalendar.DAY_OF_WEEK));
-
-		// Mes actual
-		// acordaros de sumar 1 al mes
-		assertEquals(11, (hoyDate.get(GregorianCalendar.MONTH) + 1));
-		assertEquals(GregorianCalendar.NOVEMBER,
-				(hoyDate.get(GregorianCalendar.MONTH) + 1));
-
-		// Anyo actual
-		assertEquals(2014, hoyDate.get(GregorianCalendar.YEAR));
-
-	}
-
-	@Test
-	public void testFecha() throws Exception {
-
-		// crear las cadenas para cada idioma
-		String hoyStringCastellano = hoyDate
-				.get(GregorianCalendar.DAY_OF_MONTH)
-				+ "/"
-				+ (hoyDate.get(GregorianCalendar.MONTH) + 1)
-				+ "/"
-				+ hoyDate.get(GregorianCalendar.YEAR);
-		String hoyStringEuskera = hoyDate.get(GregorianCalendar.YEAR) + "/"
-				+ (hoyDate.get(GregorianCalendar.MONTH) + 1) + "/"
-				+ hoyDate.get(GregorianCalendar.DAY_OF_MONTH);
-
-		// testear cadenas contra Fecha.getString()
-		assertEquals(hoyStringCastellano,
-				Fecha.getString(hoyDate, Idioma.CASTELLANO));
-
-		assertEquals(hoyStringEuskera, Fecha.getString(hoyDate, Idioma.EUSKARA));
 
 	}
 
