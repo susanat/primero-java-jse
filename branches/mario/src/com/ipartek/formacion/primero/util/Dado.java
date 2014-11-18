82c06d4f8d3d89bb1932a88f249420798439bd04
mario.alsol@gmail.com <mario.alsol@gmail.com@06e40e15-71bd-7a1b-1a1c-ba104c65ac7c>
2014-11-18 08:20:23 +0000

    Modificada la clase Dado, para que un nuevo metodo devuelva la tirada en INT

--- branches/mario/src/com/ipartek/formacion/primero/util/Dado.java
+++ branches/mario/src/com/ipartek/formacion/primero/util/Dado.java
@@ -2,31 +2,22 @@ package com.ipartek.formacion.primero.util;
 
 public class Dado {
 
+	static String[] alumnos = new String[] { "Susana", "Zuahitz", "Kepa",
+			"Urko", "Jon", "Sergio", "Joseba", "Mario", "Aritz", "Fran",
+			"Maitane", "Jose" };
 
-	
-	static String[] alumnos = new String[]{
-		"Susana",
-		"Zuahitz",
-		"Kepa",
-		"Urko",
-		"Jon",
-		"Sergio",
-		"Joseba",
-		"Mario",
-		"Aritz",
-		"Fran",
-		"Maitane",
-		"Jose"
-	};	
-	
-	static public void tirar (){		
-		int aleatorio = (int)(Math.random()*alumnos.length);
+	static public void tirar() {
+		int aleatorio = getTirada();
 		System.out.println(alumnos[aleatorio]);
 	}
-	
+
+	static public int getTirada() {
+		return (int) (Math.random() * alumnos.length);
+	}
+
 	public static void main(String[] args) {
 		tirar();
-		
+
 	}
-	
+
 }
