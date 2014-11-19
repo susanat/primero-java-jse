ce04450c8b20301ee6d42068d6f7580d622e63d5
josebacarr@gmail.com <josebacarr@gmail.com@06e40e15-71bd-7a1b-1a1c-ba104c65ac7c>
2014-11-19 08:25:22 +0000

    Initial Commit

---
+++ branches/joseba_migracion/src/com/ipartek/ejercicio/migracion/Persona.java
@@ -0,0 +1,13 @@
+package com.ipartek.ejercicio.migracion;
+
+public class Persona {
+
+    private String nombre;
+    private String apellido;
+    private String poblacion;
+    private int edad;
+    private String mail;
+    private String dni;
+    private String categoria;
+
+}
