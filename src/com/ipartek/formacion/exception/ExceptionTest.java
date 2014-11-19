025b1df38731be3c2f5893b56d5c0e533720004c
susanacostoya@gmail.com <susanacostoya@gmail.com@06e40e15-71bd-7a1b-1a1c-ba104c65ac7c>
2014-11-19 07:40:30 +0000

    Cambios en los ejercicios

--- src/com/ipartek/formacion/exception/ExceptionTest.java
+++ src/com/ipartek/formacion/exception/ExceptionTest.java
@@ -1,6 +1,6 @@
 package com.ipartek.formacion.exception;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -12,46 +12,45 @@ import com.ipartek.formacion.primero.bean.Persona;
 
 public class ExceptionTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void test() {
+
+	Persona p = null;
+	try {
+	    p.getApellido1();
+	    assertTrue("No deberia ejecutar esta linea", false);
+	} catch (final Exception a) {
+	    assertTrue(true);
+	} finally {
+	    assertTrue("Siempre se deberia ejecutar", true);
 	}
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
+	try {
+	    p = new Persona();
+	    p.getApellido1();
+	    assertTrue(true);
+	} catch (final Exception a) {
+	    assertTrue(true);
+	} finally {
+	    assertTrue("Siempre se deberia ejecutar", true);
 	}
 
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	@Test
-	public void test() {
-
-		Persona p = null;
-		try{
-			p.getApellido1();
-			assertTrue("No deberia ejecutar esta linea", false);
-		}catch(Exception a){
-			assertTrue(true);
-		}finally{
-			assertTrue("Siempre se deberia ejecutar",true);
-		}
-			
-		
-		try{
-			p = new Persona();
-			p.getApellido1();
-			assertTrue(true);
-		}catch(Exception a){
-			assertTrue(true);
-		}finally{
-			assertTrue("Siempre se deberia ejecutar",true);
-		}
-		
-	}
+    }
 
 }
